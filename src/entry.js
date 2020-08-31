cfc5a040a3f7c85c7b0271c86aab5153a2eb02af
jbnilles <jbnilles24@gmail.com>
2020-08-31 14:58:48 -0700

    add js entry methods for counting vowels, consanants and returning a teaser
    Co-authored-by: Joseph Karnafel <josephkarnafel@gmail.com>

---
+++ src/entry.js
@@ -0,0 +1,38 @@
+export default function Entry (title, body){
+  this.title = title;
+  this.body = body;
+}
+
+Entry.prototype.wordCount = function(){
+  let bodyArray = this.body.split(" ");
+  return bodyArray.length;
+}
+
+Entry.prototype.getVowels = function(){
+  const vowelCount = this.body.replace(/[^aeiou]/gi, "");
+  return vowelCount.length;
+}
+
+Entry.prototype.getConsonants = function(){
+  const consonantCount = this.body.replace(/[^bcdfghjklmnpqrstvwxyz]/gi, "");
+  //console.log(consonantCount.replace(/[aeiou]|\W/gi,'').length);
+  return consonantCount.length;
+  //    
+}
+//   body.replace(/[^aeiou]/gi, "") 
+//  asdfasdfgg..123
+//  aa
+
+Entry.prototype.getTeaser = function(){
+  let bodyArray = this.body.split(" ");
+  let teaser = [];
+  for(let i=0; i < 8; i++){
+    console.log(bodyArray[i].includes("."));
+    if(bodyArray[i].includes(".")){
+      teaser.push(bodyArray[i]);
+      break;
+    }
+    teaser.push(bodyArray[i]);
+  }
+  return teaser.join(" ");
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import '../css/styles.css';
 
+import $ from 'jQuery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+import Entry from './entry.js';
 $(document).ready(function() { 
-
+  let test = "Note that in our own code, we will separate shapes.";
+  let entry = new Entry('test',test);
+  $('#result').text(entry.getTeaser());
+  //console.log(entry.wordCount());
 
 });
